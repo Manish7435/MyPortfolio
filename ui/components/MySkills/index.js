e75df41fe7027859a38b4b9244b39b78e6e4639e
man-ishh <manishd.kumar@cogoport.com>
2023-11-28 02:42:30 +0530

    more skills added

--- ui/components/MySkills/index.js
+++ ui/components/MySkills/index.js
@@ -9,9 +9,11 @@ const MySkills = () => {
     return (<div className='sm:px-44 px-6 mt-16'>
         <div className='text-white mr-4 text-3xl'>My Skills</div>
         <div className='flex justify-between flex-wrap mt-8'>
-        <Pi percent={getOffsetFromPercent(60)} title={'HTML'}/>
-        <Pi percent={getOffsetFromPercent(90)} title={'Javascript'}/>
-        <Pi percent={getOffsetFromPercent(70)} title={'CSS'}/>
+        <Pi percent={getOffsetFromPercent(25)} title={'HTML'}/>
+        <Pi percent={getOffsetFromPercent(30)} title={'CSS'}/>
+        <Pi percent={getOffsetFromPercent(87)} title={'Javascript'}/>
+        <Pi percent={getOffsetFromPercent(50)} title={'ReactJS'}/>
+        <Pi percent={getOffsetFromPercent(70)} title={'NextJS'}/>
         </div>
     </div>
     )
