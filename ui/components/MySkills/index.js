856e01c946fcd29d4a4fffc35ea709597c1904ee
man-ishh <manishd.kumar@cogoport.com>
2023-11-28 03:30:19 +0530

    tailwind added

--- ui/components/MySkills/index.js
+++ ui/components/MySkills/index.js
@@ -10,10 +10,12 @@ const MySkills = () => {
         <div className='text-white mr-4 text-3xl'>My Skills</div>
         <div className='flex justify-between flex-wrap mt-8'>
         <Pi percent={getOffsetFromPercent(25)} title={'HTML'}/>
-        <Pi percent={getOffsetFromPercent(30)} title={'CSS'}/>
+        <Pi percent={getOffsetFromPercent(10)} title={'CSS'}/>
+        <Pi percent={getOffsetFromPercent(35)} title={'Tailwind'}/>
         <Pi percent={getOffsetFromPercent(87)} title={'Javascript'}/>
         <Pi percent={getOffsetFromPercent(50)} title={'ReactJS'}/>
         <Pi percent={getOffsetFromPercent(70)} title={'NextJS'}/>
+
         </div>
     </div>
     )
